22e06483e0a55342b40dac05e600736f4159b343
Michal Špaček <mail@michalspacek.cz>
2015-02-17 18:46:01 +0100

    The input and output can be mcrypt compatible now
    That is now you can use OpenSSL function even when you have the data already encrypted using mcrypt functions.

--- functions-encrypthash.php
+++ functions-encrypthash.php
@@ -2,39 +2,72 @@
 /**
  * Encrypted password hash storage functions.
  *
- * Uses bcrypt + AES-256-CBC PKCS#7 padding.
+ * Uses bcrypt + AES-256-CBC PKCS#7 padding or ZeroBytePadding for MCrypt compatibility.
  *
  * Requires OpenSSL PHP extension.
  *
  * @author Michal Špaček <https://www.michalspacek.cz>
  */
 
-function encryptAndHash($password, $keyHex)
+/**
+ * Hash a password and then encrypt the hash.
+ *
+ * @param string $password The password
+ * @param string $keyHex The hexdec key (64 chars, 0-9, A-F)
+ * @param boolean $mcryptCompatibility Whether the result should be compatible with mcrypt_encrypt
+ * @return string
+ */
+function encryptAndHash($password, $keyHex, $mcryptCompatibility = false)
 {
 	$hash = password_hash($password, PASSWORD_DEFAULT);
 
 	$cipher = 'AES-256-CBC';
 	$key = pack('H64', $keyHex);
 	$options = OPENSSL_RAW_DATA;
+	if ($mcryptCompatibility) {
+		$options |= OPENSSL_ZERO_PADDING;
+		$hash .= str_repeat("\0", 16 - (strlen($hash) % 16));
+	}
 	$iv = createIv(openssl_cipher_iv_length($cipher));
 	$encryptedHash = openssl_encrypt($hash, $cipher, $key, $options, $iv);
 	return base64_encode($iv . $encryptedHash);
 }
 
-function decryptAndVerifyHash($password, $encryptedBase64, $keyHex)
+/**
+ * Decrypt hash and verify that it matches a password.
+ *
+ * @param string $password The user's password
+ * @param string $encryptedBase64 Encrypted hash created by encryptAndHash()
+ * @param string $keyHex The encryption key
+ * @param boolean $mcryptCompatibility Whether the input is compatible with mcrypt_encrypt
+ * @return boolean
+ */
+function decryptAndVerifyHash($password, $encryptedBase64, $keyHex, $mcryptCompatibility = false)
 {
 	$encrypted = base64_decode($encryptedBase64);
 
 	$cipher = 'AES-256-CBC';
 	$key = pack('H64', $keyHex);
 	$options = OPENSSL_RAW_DATA;
+	if ($mcryptCompatibility) {
+		$options |= OPENSSL_ZERO_PADDING;
+	}
 	$ivSize = openssl_cipher_iv_length($cipher);
 	$iv = substr($encrypted, 0, $ivSize);
 	$encrypted = substr($encrypted, $ivSize);
 	$decryptedHash = openssl_decrypt($encrypted, $cipher, $key, $options, $iv);
+	if ($mcryptCompatibility) {
+		$decryptedHash = rtrim($decryptedHash, "\0");
+	}
 	return password_verify($password, $decryptedHash);
 }
 
+/**
+ * Create an initialization vector (IV).
+ *
+ * @param integer $length IV length in bytes
+ * @return string
+ */
 function createIv($length)
 {
 	$random = false;
--- test-encrypthash.php
+++ test-encrypthash.php
@@ -1,10 +1,42 @@
 <?php
 require_once './functions-encrypthash.php';
 
-$key = current(unpack('H64', mcrypt_create_iv(32, MCRYPT_DEV_RANDOM)));
+$keyHex = current(unpack('H64', mcrypt_create_iv(32, MCRYPT_DEV_URANDOM)));
 $plainText = 'Haxx0r ipsum tera dereference rsa ascii foo bypass flush ip';
-$encrypted = encryptAndHash($plainText, $key);
+
 
 echo 'Test: ';
-echo (decryptAndVerifyHash($plainText, $encrypted, $key) === true ? 'OK' : 'FAILED');
+$encrypted = encryptAndHash($plainText, $keyHex);
+echo (decryptAndVerifyHash($plainText, $encrypted, $keyHex) === true ? 'OK' : 'FAILED');
+echo PHP_EOL;
+
+if (!extension_loaded('mcrypt')) {
+	echo 'MCrypt extension not loaded, cannot test MCrypt compatibility';
+	echo PHP_EOL;
+	exit;
+}
+
+echo 'Test mcrypt compatibility, encrypt OpenSSL, decrypt MCrypt: ';
+// encrypt using OpenSSL
+$encrypted = encryptAndHash($plainText, $keyHex, true);
+// decrypt using MCrypt
+$encrypted = base64_decode($encrypted);
+$ivSize = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC);
+$iv = substr($encrypted, 0, $ivSize);
+$encrypted = substr($encrypted, $ivSize);
+$decrypted = mcrypt_decrypt(MCRYPT_RIJNDAEL_128, pack('H64', $keyHex), $encrypted, MCRYPT_MODE_CBC, $iv);
+$decrypted = rtrim($decrypted, "\0");
+echo (password_verify($plainText, $decrypted) === true ? 'OK' : 'FAILED');
+echo PHP_EOL;
+
+
+echo 'Test mcrypt compatibility, encrypt MCrypt, decrypt OpenSSL: ';
+// encrypt using MCrypt
+$hash = password_hash($plainText, PASSWORD_DEFAULT);
+$key = pack('H64', $keyHex);
+$iv = mcrypt_create_iv(mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC), MCRYPT_DEV_URANDOM);
+$encrypted = mcrypt_encrypt(MCRYPT_RIJNDAEL_128, $key, $hash, MCRYPT_MODE_CBC, $iv);
+$encrypted = base64_encode($iv . $encrypted);
+// decrypt using OpenSSL
+echo (decryptAndVerifyHash($plainText, $encrypted, $keyHex, true) === true ? 'OK' : 'FAILED');
 echo PHP_EOL;
